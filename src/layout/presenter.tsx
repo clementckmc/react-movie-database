3655b2f2d457700b866712fe26b422194be26fd5
clementchoi <clementchoi@team-lab.com>
2024-06-27 18:45:28 +0900

    feat: navbar link

--- src/layout/presenter.tsx
+++ src/layout/presenter.tsx
@@ -1,5 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 type Props = {
   children: React.ReactNode;
@@ -8,9 +9,13 @@ type Props = {
 export const LayoutPresenter: React.FC<Props> = ({children}) => {
   return (
     <>
-      <Navbar fixed="top">
+      <Navbar fixed="top" bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand>Movie Database</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/react-movie-database">
+              Movie Database
+            </Link>
+          </Navbar.Brand>
         </Container>
       </Navbar>
       <Container>{children}</Container>
