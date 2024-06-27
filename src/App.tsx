76d5c53cc3af84f3ff59b66593eedf621dcf3706
clementchoi <clementchoi@team-lab.com>
2024-06-27 18:50:09 +0900

    feat: 404 Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import { IndexPage } from './pages/IndexPage/container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { NotFoundPage } from './pages/NotFoundPage/container';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path="/react-movie-database" element={<IndexPage />}></Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   )
---
+++ src/pages/NotFoundPage/container.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../../layout/container";
+import { NotFoundPagePresenter } from "./presenter";
+
+export const NotFoundPage = () => {
+  return (
+    <Layout>
+      <NotFoundPagePresenter />
+    </Layout>
+  );
+};
