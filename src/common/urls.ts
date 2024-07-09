429e43916e75b4c89684c714cb9e27fc5e4aba46
clementchoi <clementchoi@team-lab.com>
2024-07-09 18:46:11 +0900

    feat: get poster images

---
+++ src/common/urls.ts
@@ -0,0 +1,2 @@
+export const baseURL = "https://api.themoviedb.org/3/";
+export const imageURL = "https://image.tmdb.org/t/p/w500";
--- src/components/MovieCard/presenter.tsx
+++ src/components/MovieCard/presenter.tsx
@@ -2,6 +2,7 @@ import { Button, Card } from "react-bootstrap"
 import noImage from "../../assets/no_image.jpg";
 import { ComponentProps } from "react";
 import { MovieCard } from "./container";
+import { imageURL } from "../../common/urls";
 
 type Props = Pick<ComponentProps<typeof MovieCard>, "movieDto">
 
@@ -10,7 +11,7 @@ export const MovieCardPresenter: React.FC<Props> = ({
 }: Props) => {
   return (
     <Card style={{ width: '18rem' }} className="mx-1 mb-4">
-      <Card.Img variant="top" src={noImage} alt="No Image Found" />
+      <Card.Img variant="top" src={movieDto.poster_path ? `${imageURL}/${movieDto.poster_path}` : noImage} alt="No Image Found" />
       <Card.Body>
         <Card.Title>{movieDto.title}</Card.Title>
         <Card.Text>{movieDto.release_date}</Card.Text>
