16a8c30a02773e3753f980e50663252535a0f547
clementchoi <clementchoi@team-lab.com>
2024-07-11 18:25:38 +0900

    feat: search bar dropdown (mock)

--- src/components/SearchBar/presenter.tsx
+++ src/components/SearchBar/presenter.tsx
@@ -1,15 +1,26 @@
-import { Button, Form, InputGroup } from "react-bootstrap"
+import { Button, Dropdown, Form, InputGroup } from "react-bootstrap"
 
 export const SearchBarPresenter: React.FC = () => {
   return (
     <InputGroup className="mb-3">
-    <Form.Control
-      placeholder="Type a movie name to search..."
-      aria-label="Search Movie By Name"
-    />
-    <Button variant="outline-secondary" id="button-addon2">
-      <i className="bi bi-search"></i>
-    </Button>
-  </InputGroup>
+      <Dropdown>
+        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+          English
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">English</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Japanese</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Spanish</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Form.Control
+        placeholder="Type a movie name to search..."
+        aria-label="Search Movie By Name"
+      />
+      <Button variant="outline-secondary" id="button-addon2">
+        <i className="bi bi-search"></i>
+      </Button>
+    </InputGroup>
   )
 }
