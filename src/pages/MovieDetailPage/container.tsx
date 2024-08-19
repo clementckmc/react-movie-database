4e0a330bb5e2e72a54cf29b93e1f86b1c47ce3af
clementchoi <clementchoi@team-lab.com>
2024-08-19 16:35:55 +0900

    feat: movie detail page (remove mock)

--- src/pages/MovieDetailPage/container.tsx
+++ src/pages/MovieDetailPage/container.tsx
@@ -3,12 +3,14 @@ import { Layout } from "../../layout/container";
 import { MovieDetailPagePresenter } from "./presenter";
 import { MovieDetailDto } from "../../types/MovieDetailDto";
 import axios, { AxiosResponse } from "axios";
+import { useParams } from "react-router-dom";
 
 export const MovieDetailPage: React.FC = () => {
+  const { id } = useParams();
   const [movieDetailDto, setMovieDetailDto] = useState<MovieDetailDto>();
 
   useEffect(() => {
-    axios.get("https://api.themoviedb.org/3/movie/974262", {
+    axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
       headers: {
         accept: 'application/json',
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`
