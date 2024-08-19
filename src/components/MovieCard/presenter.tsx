bd3d8e2440302b95cc8340b81d5bba19d74d26f5
clementchoi <clementchoi@team-lab.com>
2024-08-19 17:55:26 +0900

    feat: whole card as the link

--- src/components/MovieCard/presenter.tsx
+++ src/components/MovieCard/presenter.tsx
@@ -1,26 +1,24 @@
-import { Button, Card } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 import noImage from "../../assets/no_image.jpg";
 import { ComponentProps } from "react";
 import { MovieCard } from "./container";
 import { imageURL } from "../../common/urls";
-import { useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 type Props = Pick<ComponentProps<typeof MovieCard>, "movieDto">
 
 export const MovieCardPresenter: React.FC<Props> = ({
   movieDto
 }: Props) => {
-  const navigate = useNavigate();
   return (
-    <Card style={{ width: '18rem' }} className="mx-1 mb-4">
-      <Card.Img variant="top" src={movieDto.poster_path ? `${imageURL}/${movieDto.poster_path}` : noImage} alt="No Image Found" />
-      <Card.Body>
-        <Card.Title>{movieDto.title}</Card.Title>
-        <Card.Text>{movieDto.release_date}</Card.Text>
-        <Button variant="primary" onClick={() => navigate(`/react-movie-database/movie/${movieDto.id}`)}>
-          Detail Page
-        </Button>
-      </Card.Body>
-    </Card>
+    <Link to={`/react-movie-database/movie/${movieDto.id}`} style={{ textDecoration: 'none', display: 'block', width: '18rem' }} className="mx-1 mb-4">
+      <Card className="movie-card">
+        <Card.Img variant="top" src={movieDto.poster_path ? `${imageURL}/${movieDto.poster_path}` : noImage} alt="No Image Found" />
+        <Card.Body>
+          <Card.Title>{movieDto.title}</Card.Title>
+          <Card.Text>{movieDto.release_date}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   )
 }
