9288c58169823c27a6789488bfc5e8910d4e1bf1
clementchoi <clementchoi@team-lab.com>
2024-08-19 17:33:22 +0900

    feat: detail page (button)

--- src/components/MovieCard/presenter.tsx
+++ src/components/MovieCard/presenter.tsx
@@ -3,19 +3,23 @@ import noImage from "../../assets/no_image.jpg";
 import { ComponentProps } from "react";
 import { MovieCard } from "./container";
 import { imageURL } from "../../common/urls";
+import { useNavigate } from "react-router-dom";
 
 type Props = Pick<ComponentProps<typeof MovieCard>, "movieDto">
 
 export const MovieCardPresenter: React.FC<Props> = ({
   movieDto
 }: Props) => {
+  const navigate = useNavigate();
   return (
     <Card style={{ width: '18rem' }} className="mx-1 mb-4">
       <Card.Img variant="top" src={movieDto.poster_path ? `${imageURL}/${movieDto.poster_path}` : noImage} alt="No Image Found" />
       <Card.Body>
         <Card.Title>{movieDto.title}</Card.Title>
         <Card.Text>{movieDto.release_date}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary" onClick={() => navigate(`/react-movie-database/movie/${movieDto.id}`)}>
+          Detail Page
+        </Button>
       </Card.Body>
     </Card>
   )
