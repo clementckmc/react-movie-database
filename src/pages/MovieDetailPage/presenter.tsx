c502d9806c3c31555d1d4ab25db6c6f82f027e61
clementchoi <clementchoi@team-lab.com>
2024-08-19 15:56:37 +0900

    feat: basic html/css of movie detail page

--- src/pages/MovieDetailPage/presenter.tsx
+++ src/pages/MovieDetailPage/presenter.tsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap"
+import { Container, Stack } from "react-bootstrap"
 import { MovieDetailDto } from "../../types/MovieDetailDto"
 import noImage from "../../assets/no_image.jpg";
 import { imageURL } from "../../common/urls";
@@ -14,14 +14,18 @@ export const MovieDetailPagePresenter = ({
       <Container>
         {
           movieDetailDto ?
-          <Container>
-            <h1>{movieDetailDto.title}</h1>
-            <img src={movieDetailDto.poster_path ? `${imageURL}/${movieDetailDto.poster_path}` : noImage} alt="No Image Found" />
-            <p>{movieDetailDto.overview}</p>
-            <p>Release Date: {movieDetailDto.release_date}</p>
-            <p>Runtime: {movieDetailDto.runtime} minutes</p>
-            <p>Genres: {movieDetailDto.genres.map(genre => genre.name).join(", ")}</p>
-          </Container> :
+          <Stack direction="horizontal" gap={3}>
+            <div className="p-2">
+              <img src={movieDetailDto.poster_path ? `${imageURL}/${movieDetailDto.poster_path}` : noImage} alt="No Image Found" width="100%" height="100%" />
+            </div>
+            <div className="p-2 movie-detail-text">
+              <h1>{movieDetailDto.title}</h1>
+              <p><i>{movieDetailDto.overview}</i></p>
+              <p><strong>Release Date:</strong> {movieDetailDto.release_date}</p>
+              <p><strong>Runtime:</strong> {movieDetailDto.runtime} minutes</p>
+              <p>Genres: {movieDetailDto.genres.map(genre => genre.name).join(", ")}</p>
+            </div>
+          </Stack> :
           <h2>No movie found</h2>
         }
       </Container>
