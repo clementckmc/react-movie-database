55db66156b77013660a24c2974ac3f00b44e3acb
clementchoi <clementchoi@team-lab.com>
2024-09-07 13:02:10 +0900

    fix: movie card size:

--- src/components/MovieCard/presenter.tsx
+++ src/components/MovieCard/presenter.tsx
@@ -11,8 +11,8 @@ export const MovieCardPresenter: React.FC<Props> = ({
   movieDto
 }: Props) => {
   return (
-    <Link to={`/react-movie-database/movie/${movieDto.id}`} style={{ textDecoration: 'none', display: 'block', width: '18rem' }} className="mx-1 mb-4">
-      <Card className="movie-card">
+    <Link to={`/react-movie-database/movie/${movieDto.id}`} style={{ textDecoration: 'none', display: 'block', width: '18rem'}} className="mx-1 mb-4">
+      <Card className="movie-card" style={{ height: '30rem' }}>
         <Card.Img variant="top" src={movieDto.poster_path ? `${imageURL}/${movieDto.poster_path}` : noImage} alt="No Image Found" />
         <Card.Body>
           <Card.Title>{movieDto.title}</Card.Title>
